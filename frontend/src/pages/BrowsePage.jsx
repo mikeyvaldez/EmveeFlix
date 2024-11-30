1f87e63b8f3839d711b96dbdfb8a3894f4e8084d
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-11-30 11:04:32 -0800

    edit pages

--- frontend/src/pages/BrowsePage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function BrowsePage() {
-  return (
-    <div>BrowsePage</div>
-  )
-}
--- frontend/src/pages/WatchPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function WatchPage() {
-  return (
-    <div>WatchPage</div>
-  )
-}
