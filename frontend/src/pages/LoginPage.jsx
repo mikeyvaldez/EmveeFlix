c9f9e040f01ff6fadcf47a0454710acd0f8076b4
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-11-30 14:34:09 -0800

    login page structure

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,4 @@
-import Header from "../components/Header"
-
-
+import Header from "../components/Header";
 
 export default function LoginPage() {
   return (
@@ -8,42 +6,47 @@ export default function LoginPage() {
       <Header />
       <div className="flex justify-center items-center h-full">
         <div className="bg-black bg-opacity-70 p-16 self-center mt-2 w-full max-w-md rounded-md">
-          <h2 className="text-white text-4xl mb-8 font-semibold">
-            Sign up
-          </h2>
-          
-            <form
-              className="flex flex-col gap-4"              
-            >
-              <input id="username" type="text" label="Username" name="name" />
-              <input
-                id="email"
-                type="email"
-                label="Email address"
-                name="email"
-              />
-              <input />
-              <input
-                type="submit"
-                className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-800"
-              />              
-            </form>          
-            <p
-              className="text-neutral-500 mt-12"              
-            >
-              <span className="text-white ml-1 hover:underline cursor-pointer">
-                First time using Netflix?
-              </span>
-            </p>          
-            <p
-              className="text-neutral-500 mt-12"              
-            >
-              <span className="text-white ml-1 hover:underline cursor-pointer">
-                Already have an account?
-              </span>
-            </p>          
+          <h2 className="text-white text-4xl mb-8 font-semibold">Sign up</h2>
+
+          <form className="flex flex-col gap-4">
+            <input
+              id="username"
+              type="text"
+              placeholder="Username"
+              name="name"
+              className="block rounded-md px-6 pt-6 pb-1 w-full text-md text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer invalid:border-b-1"
+            />
+            <input
+              id="email"
+              type="email"
+              placeholder="Email address"
+              name="email"
+              className="block rounded-md px-6 pt-6 pb-1 w-full text-md text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer invalid:border-b-1"
+            />
+            <input
+              id="password"
+              type="password"
+              placeholder="Password"
+              name="password"
+              className="block rounded-md px-6 pt-6 pb-1 w-full text-md text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer invalid:border-b-1"
+            />
+            <input
+              type="submit"
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-800"
+            />
+          </form>
+          <p className="text-neutral-500 mt-5">
+            <span className="text-white ml-1 hover:underline cursor-pointer">
+              First time using Netflix?
+            </span>
+          </p>
+          <p className="text-neutral-500 mt-5">
+            <span className="text-white ml-1 hover:underline cursor-pointer">
+              Already have an account?
+            </span>
+          </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
