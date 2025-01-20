9cfd318c429c0c6cc3b3ebb361a86f5c0ad30829
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-19 22:54:07 -0800

    edit to emveeflix

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { signup, signin, google } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+// the functions are built in the controllers directory then imported into this file.
+// helps keep the code clean.
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.post("/google", google)
+
+export default router;
--- client/src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export interface Movie {
-  id: string;
-  title: string;
-  description: string;
-  thumbnailUrl: string;
-  videoUrl: string;
-  genre: string;
-  duration: string;
-}
