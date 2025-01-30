29397a741e9003cb87087e9e1926df9946cf5290
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-29 21:42:16 -0800

    production build

--- server/index.js
+++ server/index.js
@@ -4,10 +4,6 @@ import authRoutes from "./routes/auth.js";
 import moviesRoutes from "./routes/movies.js";
 import subRoutes from "./routes/sub.js";
 import dotenv from "dotenv";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path';
-
 
 
 dotenv.config({ path:"../.env" });
@@ -23,15 +19,6 @@ app.use(cors());
 app.use("/api", moviesRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/sub", subRoutes);
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-app.use(express.static(path.join(__dirname, "client", "dist")))
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
 
 
 app.listen(port, () => {
