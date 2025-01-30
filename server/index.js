0acd5b268249439fef6286971f4b752187e386c5
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-29 21:21:19 -0800

    edit to index for production

--- server/index.js
+++ server/index.js
@@ -13,14 +13,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.get("/", (req, res) => {
-//   return res.send("HELLO WORLD");
-// });
+
+app.listen(port, () => {
+  console.log(`Now listening on PORT ${port}`);
+});
+
 
 app.use("/api", moviesRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/sub", subRoutes);
 
-app.listen(port, () => {
-  console.log(`Now listening on PORT ${port}`);
+app.use(express.static(path.join(__dirname, "/client/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
